5306488170a9c4d5b934c9e4171633bbf77529c5
rmanzuk <42247790+rmanzuk@users.noreply.github.com>
2021-02-18 16:38:57 -0500

    modified to take user input

--- archaeo_morphology_clicking/remove_spurious_edges.m
+++ archaeo_morphology_clicking/remove_spurious_edges.m
@@ -1,38 +1,83 @@
-function [cleaned_outers] = remove_spurious_edges(initial_outers,nslice_cutoff)
-% This function goes through and removes edges that don't appear on enough
-% slices, and so must be considered spurious from the automated tracing
-% output.
+function [cleaned_outers] = remove_spurious_edges(initial_outers)
+% This function goes through and removes edges that are outside the ranges
+% specified through user inputs on the x-y plane. It helps to get rid of
+% elements from the scan that are parts of the coral housing.
 %
 % IN
 % initial_outers: 1xn_slice cell array containing the edge tracings from
 % the automated tracing pipeline
 %
-% nslice_cutoff: Number of slices appearing on, below which an edge is
-% considered spurious. For example, if you want to consider all traces
-% appearing on less than 5 slices, set this value to 5.
 %
 % OUT
 % cleaned_outers: 1xn_slice cell array containing the edge tracings from
-% the automated tracing pipeline, with those not appearing on enough slices
-% removed
+% the automated tracing pipeline, with bad edges removed
 %
 %
 % R. A. Manzuk 12/21/2020
+% edited
     %% begin the function
-    % get some initial info
-    total_edges = max(cellfun(@numel,initial_outers));
-    not_empty_ind = zeros(size(initial_outers,2),total_edges);
-    
-    %figure out which edges are not present in which slices
-    for i = 1:numel(initial_outers)
-        not_empty_ind(i,1:numel(initial_outers{i})) = ~cellfun(@isempty,initial_outers{i});
+    % plot a subset of the edges on just an x-y projection to show user
+    % which edges they may not want to include
+    figure();
+    for i = [1:50:numel(initial_outers)]
+        for j = 1:numel(initial_outers{i})
+            if ~isempty(initial_outers{i}{j})
+                scatter(initial_outers{i}{j}(:,1),initial_outers{i}{j}(:,2))
+                hold on
+            end
+        end
     end
     
-    %number of slices appearing is just the sum of our not_empty matrix
-    not_enough_slices = sum(not_empty_ind) <= nslice_cutoff;
+    % ask the user to identify the cutoff points
+    disp('click the spot in the upper left where only spurious branches are up and to the left');
+    [x_thresh_ul, y_thresh_ul] = ginput(1);
+    disp('click the spot in the upper right where only spurious branches are up and to the right');
+    [x_thresh_ur, y_thresh_ur] = ginput(1);
+    disp('click the spot in the lower left where only spurious branches are down and to the left');
+    [x_thresh_ll, y_thresh_ll] = ginput(1);
+    disp('click the spot in the lower right where only spurious branches are down and to the right');
+    [x_thresh_lr, y_thresh_lr] = ginput(1);
+   
+
+    % now go through the array and figure out which should be excluded
+    % based upon thresholds
+    % set up empty id system for bad edges
+    to_purge = zeros(numel(initial_outers),max(cellfun(@numel,initial_outers)));
+    % and an array to keep track of what is empty so we can get rid of
+    % edges that just aren't there at all for some reason
+    empty_inds = ones(numel(initial_outers),max(cellfun(@numel,initial_outers)));
     
+    for i = 1:numel(initial_outers)
+        for j = 1:numel(initial_outers{i})
+            if ~isempty(initial_outers{i}{j})
+                if any(initial_outers{i}{j}(:,1) < x_thresh_ul & initial_outers{i}{j}(:,2) > y_thresh_ul)
+                    to_purge(i,j) = 1;
+                elseif any(initial_outers{i}{j}(:,1) > x_thresh_ur & initial_outers{i}{j}(:,2) > y_thresh_ur)
+                    to_purge(i,j) = 1;
+                elseif any(initial_outers{i}{j}(:,1) < x_thresh_ll & initial_outers{i}{j}(:,2) < y_thresh_ll)
+                    to_purge(i,j) = 1;
+                elseif any(initial_outers{i}{j}(:,1) > x_thresh_lr & initial_outers{i}{j}(:,2) < y_thresh_lr)
+                    to_purge(i,j) = 1;
+                end
+            else
+                empty_inds(i,j) = 0;
+            end
+        end 
+    end
+    
+    % Any edges identified as needing to be purged will have a column sum
+    % greater than 0
+    bad_edge = sum(to_purge) > 0;
+    % any edges that are completely empty will have column sum of 0 in the
+    % empty inds matrix
+    empty_edge = sum(empty_inds) == 0;
+    
+    % combine the two above so we know who to get rid of 
+    get_rid_inds = or(bad_edge,empty_edge);
+    
+    % initiate cleaned outers
     % apply the cutoff
     for i = 1:numel(initial_outers)
-        cleaned_outers{i} = initial_outers{i}(~not_enough_slices(1:numel(initial_outers{i})));
+        cleaned_outers{i} = initial_outers{i}(~get_rid_inds(1:numel(initial_outers{i})));
     end
 end
