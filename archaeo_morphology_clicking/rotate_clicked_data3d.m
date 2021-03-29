b4838d37c63d5bf31244957f0249ec1b36d956be
Ryan <42247790+rmanzuk@users.noreply.github.com>
2021-03-29 13:12:03 -0400

    can handle empty cells:

--- archaeo_morphology_clicking/rotate_clicked_data3d.m
+++ archaeo_morphology_clicking/rotate_clicked_data3d.m
@@ -1,15 +1,12 @@
-function [inner_3d_rotated, outer_3d_rotated] = rotate_clicked_data3d(inners, outers, block_top_sd, strike_im_heading, bedding_sd,plt)
+function [data_rotated] = rotate_clicked_data3d(data_3d, block_top_sd, strike_im_heading, bedding_sd)
 % This function takes inner and outer clicked data for archaeo branches
 % from a GIRI image stack (could be used for other 3D data), along with
 % orientation information about the block and field site to rotate the data
 % into paleo-gravitational orientation. 
 %
 % IN
-% inners: 1xn_archaeos cell array containing the densified or non-densified 3d outputs for
-% inner clicked data from the densify_3d or make_clicking_3d functions.
-%
-% outers: 1xn_archaeos cell array containing the densified or non-densified 3d outputs for
-% outer clicked data from the densify_3d or make_clicking_3d functions.
+% data_3d: 1xn_branch cell array 3d point data of each branch. Could be
+% center lines or outlines.
 %
 % block_top_sd: 1x2 array containing stike-dip orientation information for
 % the top surface of the block or image plane [strike, dip].
@@ -21,17 +18,10 @@ function [inner_3d_rotated, outer_3d_rotated] = rotate_clicked_data3d(inners, ou
 %
 % bedding_sd: 1x2 array containing strike-diop orientation information from the field site of
 % the plane of paleo-horizontal (bedding).... [strike, dip].
-%
-% plt: logical flag if the user would like the 3D rotated data plotted at
-% the completion of rotation. 1 for plot, 0 for don't plot
 % 
 % OUT
-% inner_3d_rotated: 1xn_archaeo cell array where each cell contains the 3D
-% coordinates for all inner clicked points for a given archaeo. The fourth
-% column in the cell is the button data, which may be useful later
-%
-% outer_3d_rotated: 1xn_archaeo cell array where each cell contains the 3D
-% coordinates for all outer clicked points for a given archaeo. The fourth
+% data_rotated: 1xn_branch cell array where each cell contains the 3D
+% coordinates for all input data rotated based upon input orientations. The fourth
 % column in the cell is the button data, which may be useful later
 %
 % R. A. Manzuk, 07/24/2020
@@ -55,50 +45,26 @@ function [inner_3d_rotated, outer_3d_rotated] = rotate_clicked_data3d(inners, ou
         0, 1, 0; -sind(bedding_sd(2)), 0, cosd(bedding_sd(2))];
 
     % go through, make slice data 3d, and rotate it
-    inner_3d_rotated = {};
-    for i = 1:numel(inners)
-        % extract 3d dense data for this archaeo
-        current_points = inners{i};
-        % and do all the rotations
-        rotated1 = z_rot_mat * [current_points(:,1),current_points(:,2),current_points(:,3)]';
-        rotated2 = y_rot_mat * rotated1;
-        rotated3 = z_rot_mat2 * rotated2;
-        rotated4 = y_rot_mat2 * rotated3;
-        if size(current_points,2) == 4
-            final_mat = [rotated4',current_points(:,4)];
+    data_rotated = {};
+    for i = 1:numel(data_3d)
+        if ~isempty(data_3d{i})
+            % extract 3d dense data for this archaeo
+            current_points = data_3d{i};
+            % and do all the rotations
+            rotated1 = z_rot_mat * [current_points(:,1),current_points(:,2),current_points(:,3)]';
+            rotated2 = y_rot_mat * rotated1;
+            rotated3 = z_rot_mat2 * rotated2;
+            rotated4 = y_rot_mat2 * rotated3;
+            if size(current_points,2) == 4
+                final_mat = [rotated4',current_points(:,4)];
+            else
+                final_mat = rotated4';
+            end
+            data_rotated{i} = final_mat;
         else
-            final_mat = rotated4';
+            data_rotated{i} = [];
         end
-        inner_3d_rotated{i} = final_mat;
     end
-
-    % same stuff for the outers data
-    outer_3d_rotated = {};
-    for i = 1:numel(outers)
-        current_points = outers{i};
-        % all the rotations
-        rotated1 = z_rot_mat * [current_points(:,1),current_points(:,2),current_points(:,3)]';
-        rotated2 = y_rot_mat * rotated1;
-        rotated3 = z_rot_mat2 * rotated2;
-        rotated4 = y_rot_mat2 * rotated3;
-        if size(current_points,2) == 4
-            final_mat = [rotated4',current_points(:,4)];
-        else
-            final_mat = rotated4';
-        end
-        outer_3d_rotated{i} = final_mat;
-    end
-
-if plt
-    for i = 1:numel(outer_3d_rotated)
-        scatter3(outer_3d_rotated{i}(:,1),outer_3d_rotated{i}(:,2),outer_3d_rotated{i}(:,3),5,'b','filled')
-        hold on
-        scatter3(inner_3d_rotated{i}(:,1),inner_3d_rotated{i}(:,2),inner_3d_rotated{i}(:,3),5,'r','filled')
-    end
-else
-    % do nothing
-end
-
 end
 
 
